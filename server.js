ef080bf542f0a54fc6f6adf705bc44a21b7a9636
mike <mikerah@gmail.com>
2018-11-29 05:53:31 -0600

    working

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const request = require('request');
 const argv = require('yargs').argv;
 const fs = require('fs');
 
-
-
 let data = JSON.parse(fs.readFileSync('config.json'));
-
 
 const app = express();
 
@@ -16,22 +13,19 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
 
-
-
-
-
 app.get('/', function (req, res) {
     res.render('index', {weather: null, error: null});
   })
 
 app.post('/', function(req, res){
     
-    let city = argv.c || 'neworleans';
-    let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${data["apiKey"]}';
+    let city = argv.c || 'new orleans';
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +'&APPID='+ data["apiKey"];
+    console.log(url);
 
     request (url, function(err, response, body){
         if(err){
-            console.log('error: ', err);
+            //console.log('error: ', err);
             res.render('index', {weather: null, error: "Error, please try again"});
             console.log('Error: ', err);
         } else {
@@ -43,16 +37,17 @@ app.post('/', function(req, res){
             } else if (weather.cod == '401'){
                 res.render('index', {weather: null, error: "Error, please try again"});
                 console.log('Error: invalide API key');
+                console.log("weather is : " + weatherText);
             } else {
-                let weatherText = "It's ${weather.main.temp} degrees in ${weather.name}!";
+                console.log(weather);
+                let temp = ((weather.main.temp - 273.15) * 9/5 + 32).toFixed(0);
+                let weatherText = 'It is ' + temp + ' degrees in ' + weather.name + "!";
                 res.render('index', {weather: weatherText, error: null});
                 console.log('what you got is:  ', weatherText);
             }
         }
     });
 });
-
-
 
 app.listen(3000, function() {
     console.log('Weather App listening on 3000');
